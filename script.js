3265b192650204676b4011de2e988f355cb9ed69
rosean3 <90877577+rosean3@users.noreply.github.com>
2022-05-23 20:38:36 -0300

    hotfix(main): optimize the js code

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-let backgrounds = ["background1", "background2", "background3"];
+const backgrounds = ["background1", "background2", "background3"];
+const textos = ["Background:  #292929", "Background:  #F2F2F2", "Background:  #C7C7C7"];
 
-let body = window.document.querySelector("#corpo");
-body.classList.add("background1");
+const body = window.document.querySelector("#corpo");
 const botao = window.document.querySelector("#botao");
+
 botao.addEventListener('click', () => {
-    if(body.classList.contains(backgrounds[0])){
-        body.classList.remove(backgrounds[0]);
-        body.classList.add(backgrounds[(1)]);
-        document.querySelector("#texto-seguinte").innerHTML = "Background:  #F2F2F2"
-    }
-    else if(body.classList.contains(backgrounds[1])){
-        body.classList.remove(backgrounds[1]);
-        body.classList.add(backgrounds[(2)]);
-        document.querySelector("#texto-seguinte").innerHTML = "Background:  #C7C7C7"
-    }
-    else if(body.classList.contains(backgrounds[2])){
-        body.classList.remove(backgrounds[2]);
-        body.classList.add(backgrounds[(0)]);
-        document.querySelector("#texto-seguinte").innerHTML = "Background:  #292929"
+    for (i=0; i<3; i++){
+        if(body.classList.contains(backgrounds[i])){
+            body.classList.remove(backgrounds[i]);
+            body.classList.add(backgrounds[(i+1)%3]);
+            document.querySelector("#texto-seguinte").innerHTML = textos[(i+1)%3];
+            break;
+        }
     }
 })
